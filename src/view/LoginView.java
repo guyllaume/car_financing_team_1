e3581b045d2f4a38996a32845e6ecb0f26b4e924
Jade Leblanc <jadeleblanc0@hotmail.com>
2024-02-27 22:59:06 -0500

    Update LoginView.java
    Le bouton retour a la page fonctionne toujours pas
    jai commit pareil pour que tu vois au moins et jai pas pris la peine de faire une branche ici pcq y avait rien

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -1,11 +1,54 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginView extends JPanel {
+public class LoginView extends PictureView {
+    private RetourALaPagePrincipale retourALaPagePrincipaleListener;
+    private JButton btnRetour;
+    private JLabel usernameLabel;
+    private JTextField usernameField;
+    private JLabel passwordLabel;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+    private JLabel retourLabel;
+
     public LoginView() {
-        // Ajoutez vos composants de connexion ici
-        add(new JLabel("Page de Connexion"));
+        super("src/view/Voiture.jpg");
+        setLayout(new GridLayout(5, 2));
 
+        usernameLabel = new JLabel("Email:");
+        usernameField = new JTextField();
+        add(usernameLabel);
+        add(usernameField);
+
+        passwordLabel = new JLabel("Password:");
+        passwordField = new JPasswordField();
+        add(passwordLabel);
+        add(passwordField);
+
+        loginButton = new JButton("Login");
+        add(loginButton);
+
+        btnRetour = new JButton("Retour à la page d'accueil");
+        add(btnRetour);
+
+
+        btnRetour.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (retourALaPagePrincipaleListener != null) {
+                    retourALaPagePrincipaleListener.retournerAlaPagePrincipale(); // Notify listener
+                }
+            }
+        });
+
+        setPreferredSize(new Dimension(800, 400));
     }
+
+    public interface RetourALaPagePrincipale {
+        void retournerAlaPagePrincipale();
+    }
+
 }
