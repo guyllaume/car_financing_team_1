71da01333062457ecf1869c84c1b794206467c03
guyllaume beaudry <guyllaumebeaudry@gmail.com>
2024-03-04 17:39:01 -0500

    Creation de la methode getAllPersons() de PersonDAOImpl

--- src/DAO/PersonDAOImpl.java
+++ src/DAO/PersonDAOImpl.java
@@ -5,7 +5,11 @@ import model.Person;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class PersonDAOImpl implements PersonDAO {
@@ -31,11 +35,32 @@ public class PersonDAOImpl implements PersonDAO {
     }
 
     public void deletePerson(Person person) {
-        // Implementation
+        //No Use as it will be done through Client
     }
 
     public List<Person> getAllPersons() {
-        // Implementation
-        return null;
+        String SQL_SELECT = "SELECT * FROM person;";
+        List<Person> Persons = new ArrayList<>();
+
+        try (Connection conn = PostgresSQLConfig.connect();
+             PreparedStatement statement = conn.prepareStatement(SQL_SELECT)) {
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+
+                Person person = new Person(name, age);
+                Persons.add(person);
+            }
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return Persons;
     }
 }
